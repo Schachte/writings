c5adb6ab0bdb59506e9e6ef5d3f9bf04f5773a9a
Ryan Schachte <code@ryan-schachte.com>
2022-01-12 09:18:18 -0700

    Updating SEO

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import Navigation from "@/components/Navigation";
-import Hero from "@/components/Hero";
+import Head from "next/head";
+
+import Navigation from "../components/Navigation";
+import Hero from "../components/Hero";
 import AllPosts from "@/components/AllPosts";
 import Footer from "@/components/Footer";
-import fs from "fs"
-import path from "path"
+import fs from "fs";
+import path from "path";
 import matter from "gray-matter";
-
 
 function getSortedPostsData() {
   const fileNames = fs.readdirSync("articles");
@@ -33,16 +34,28 @@ export async function getStaticProps() {
 
 export default function HomePage({ postFrontMatter }) {
   return (
-    <div className="wrapper">
-      <div className="content-wrapper">
-        <Navigation />
-        <Hero />
-        <AllPosts postMetadata={postFrontMatter} />
-        <Footer>
-          Site Built &amp; Maintained By: Ryan Schachte // Logo Design By:
-          Alyssa Sopanarat
-        </Footer>
+    <>
+      <Head>
+        <title>Schachte's Playground</title>
+        <meta charset="utf-8"></meta>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="author" content="Ryan Schachte" />
+        <meta
+          name="description"
+          content="A blog covering engineering, finance, algorithms, computer science and technology. Ryan Schachte is an avid learner, teacher and engineer. Articles covering interview prep, leetcode, programming and design."
+        />
+      </Head>
+      <div className="wrapper">
+        <div className="content-wrapper">
+          <Navigation />
+          <Hero />
+          <AllPosts postMetadata={postFrontMatter} />
+          <Footer>
+            Site Built &amp; Maintained By: Ryan Schachte // Logo Design By:
+            Alyssa Sopanarat
+          </Footer>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
